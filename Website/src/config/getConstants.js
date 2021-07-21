66e9a2e7de69cbdfd536ab1e1b9ad5b0d51d8004
KanGret <52218326+kkrgzz@users.noreply.github.com>
2021-07-21 14:47:51 +0300

    Notes Page Designed
    I designed notes page.

--- Website/src/config/getConstants.js
+++ Website/src/config/getConstants.js
@@ -1,6 +1,6 @@
-const constantsJSON = '[{"webSiteTitle" : "noteX"}]';
+const constantsJSON = '[{"webSiteTitle"    : "noteX","loginPageTitle"  : "noteX - Login","signupPageTitle" : "noteX - Signup"}]';
 
 var constants = JSON.parse(constantsJSON);
 
 // Change the window title with javascript.
-document.title = constants[0].webSiteTitle;
+document.title = constants[0].loginPageTitle;
