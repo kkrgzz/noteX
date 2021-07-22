ec1f7f73578b6d969c8dca7c063eaf03e3aef562
KanGret <52218326+kkrgzz@users.noreply.github.com>
2021-07-22 23:26:08 +0300

    I tried to add "Add Note" page.
    I tried to add "Add Note" page.

--- Website/index.php
+++ Website/index.php
@@ -9,6 +9,25 @@
 </head>
 <body>
 
+  <?php
+  if(isset($_GET['p'])){
+    switch ($_GET['p']) {
+      case 'login':
+        header( "refresh:0.1; url=login/" );
+        break;
+
+      case 'signup':
+        header( "refresh:0.1; url=signup/" );
+        break;
+
+      default:
+        header( "refresh:0.1; url=login/" );
+        break;
+    }
+  }else{
+    header( "refresh:0.1;url=login/");
+  }
+   ?>
 
   <script src="Modules/bootstrap/js/bootstrap.min.js" charset="utf-8"></script>
   <script type="text/javascript" src="Modules/jquery/jquery-3.6.0.min.js"></script>
