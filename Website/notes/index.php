c14c2b2ae9a59287b9df0da54d931cbaf1702274
KanGret <52218326+kkrgzz@users.noreply.github.com>
2021-08-03 06:53:36 +0300

    Update index.php

--- Website/notes/index.php
+++ Website/notes/index.php
@@ -1,11 +1,30 @@
 <?php
+ob_start();
 session_start();
 include("../src/config/constants.php");
 
 if(isset($_SESSION['userId']) && isset($_SESSION['username']) && isset($_SESSION['email'])){
     if(!empty($_SESSION['userId']) && !empty($_SESSION['username']) && !empty($_SESSION['email'])){
         include("notes.html");
-    }else die(EXITTEXT);
-}else die(EXITTEXT);
+
+        if(isset($_POST['logout'])){
+            if($_POST['logout'] == "true"){
+                header( "refresh:0.1; url=../login/" );
+                session_destroy();
+            }
+        }
+    }else {
+        session_destroy();
+        header( "refresh:4; url=../login/" );
+        die(EXITTEXT); 
+        
+    }
+}else {
+    session_destroy();
+    header( "refresh:4; url=../login/" );
+    die(EXITTEXT);
+    
+}
+ ob_end_flush();
 
 ?>
