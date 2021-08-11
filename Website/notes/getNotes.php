bf2aa98b2aa8a0663d3821de10cd1535e24348fc
KanGret <52218326+kkrgzz@users.noreply.github.com>
2021-08-11 03:42:34 +0300

    Update getNotes.php
    Note details shown on the modal by Bootstrap 5. I save the data in prop of button element.

--- Website/notes/getNotes.php
+++ Website/notes/getNotes.php
@@ -9,7 +9,7 @@ include($db_location);
 $conn = new dbProcess();
 
 $userId = $_SESSION['userId'];
-  
+
 $db = $conn->connDB();
 $query = $db->query("SELECT * FROM {$notesTableName} WHERE noteOwner = '$userId' ORDER BY noteId DESC ", PDO::FETCH_ASSOC);
 $queryCount = $query->rowCount();
@@ -23,15 +23,29 @@ foreach ($query as $row) {
 <!--
 \\\START///
 Note content here, the number and text is shown here. -->
-<div class="row">
+<div class="note-block row">
     <div class="notes100-number col-1 fs-30 d-flex align-items-center">
         <?php echo $count; ?>
     </div>
     <div class="notes100 col-9">
         <?php echo $row['noteTitle']; ?>
     </div>
-    <div class="notes100-pencil fs-30 col-2 d-flex align-items-center">
-        <i class="fa fa-pencil" aria-hidden="true"></i>
+    <div class="fs-30 col-2 d-flex align-items-center">
+        
+        <button 
+        class="noteDetailsButton"
+        data-bs-toggle="modal" 
+        data-bs-target="#noteDetailModal" 
+        noteDetailsID="<?php echo $row['noteId']; ?>"
+        noteDetailsOwner="<?php echo $row['noteOwner']; ?>"
+        noteDetailsTitle="<?php echo $row['noteTitle']; ?>"
+        noteDetailsContent="<?php echo $row['noteContent']; ?>"
+        noteDetailsDate="<?php echo $row['noteDate']; ?>">
+            
+            <i class="fa fa-superpowers" aria-hidden="true"></i>
+        
+        </button>
+
     </div>
 </div>
 <!-- \\\FINISH/// -->
@@ -52,3 +66,5 @@ endif;
 
 }
 ?>
+
+
